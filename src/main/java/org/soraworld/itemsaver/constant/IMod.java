d3b4b5ca923ef6f958246228f67065c80c8f0fe8
Himmelt <master@void-3.cn>
2017-06-11 13:58:40 +0800

    command bug

--- src/main/java/org/soraworld/itemsaver/constant/IMod.java
+++ src/main/java/org/soraworld/itemsaver/constant/IMod.java
@@ -8,7 +8,7 @@ import net.minecraft.util.text.TextFormatting;
 public final class IMod {
     public static final String NAME = "ItemSaver";
     public static final String MODID = "itemsaver";
-    public static final String VERSION = "1.11.2-1.1.2";
+    public static final String VERSION = "1.11.2-1.1.3";
     public static final String ACMCVERSION = "[1.11.2]";
     public static final Style YELLOW = new Style().setColor(TextFormatting.YELLOW);
     public static final Style AQUA = new Style().setColor(TextFormatting.AQUA);
